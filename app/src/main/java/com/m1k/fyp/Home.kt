1a4fa099407c893cadac2eaf9088e56f88182b22
M1K <mikepress88@gmail.com>
2019-02-05 01:18:01 +0000

    Create TableLayout for button + add 2 placeholder icons.

--- app/src/main/java/com/m1k/fyp/Home.kt
+++ app/src/main/java/com/m1k/fyp/Home.kt
@@ -3,7 +3,7 @@ package com.m1k.fyp
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.content_home.*
 
 class Home : AppCompatActivity() {
 
