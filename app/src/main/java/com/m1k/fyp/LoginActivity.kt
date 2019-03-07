7dd33cf87a7b907ac584f8dd66b44f7acca8cb6f
M1K <mikepress88@gmail.com>
2019-03-07 04:02:52 +0000

    Changed login interface to utilise recyclerViews for neater code footprint + better visuals

--- app/src/main/java/com/m1k/fyp/LoginActivity.kt
+++ app/src/main/java/com/m1k/fyp/LoginActivity.kt
@@ -4,126 +4,139 @@ import android.app.Activity
 import android.os.AsyncTask
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.CardView
-import android.widget.*
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_login.*
 
-/**
- * A login screen that offers login via email/password.
- */
+private var layoutManager: RecyclerView.LayoutManager? = null
+private var adapter: RecyclerView.Adapter<LoginActivity.Companion.RecyclerAdapter.ViewHolder>? = null
+
+
 class LoginActivity : AppCompatActivity() {
-    /**
-     * Keep track of the login task to ensure we can cancel it if requested.
-     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         // Set up the login form.
 
 
+        delButt.setOnClickListener {
+            val d = DropAll(this).execute()
+
+            d.get()
+            Toast.makeText(this, "All Users Dropped", Toast.LENGTH_SHORT).show()
+            setResult(Activity.RESULT_OK)
+            finish()
+        }
+
         email_sign_in_button.setOnClickListener {
             val newUser =
                 User(null, new_user.text.toString(), false, false, false, false, "test.png", Calender(), Week())
 
-            val c = CreateNewUser().execute(newUser)
+            val c = CreateNewUser(this).execute(newUser)
 
             c.get()
-            Toast.makeText(this, "User " + newUser.uName + " Created!", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "User $newUser.uName  Created!", Toast.LENGTH_SHORT).show()
+            GlobalApp.setLogged(newUser.uName)
+            setResult(Activity.RESULT_OK)
             finish()
         }
-        val c = CheckSize().execute()
+        val c = CheckSize(this).execute()
+
 
         if (c.get() > 0) {
+            layoutManager = LinearLayoutManager(this)
+            users_list_view.layoutManager = layoutManager
 
-            val u = GetAllUsers().execute()
+            adapter = RecyclerAdapter(this)
+            users_list_view.adapter = adapter
+        }
+    }
 
+    companion object {
+        class GetAllUsers(private val loginActivity: LoginActivity) : AsyncTask<Void, Int, List<User>>() {
+            val db = UserDataBase.getDatabase(loginActivity, null).userDataDao()
+            override fun doInBackground(vararg params: Void?): List<User> {
+                return db.getAll()
+            }
+        }
 
-            //2 per room...
-            var isFirstRow = true
+        class DropAll(private val loginActivity: LoginActivity) : AsyncTask<Void, Int, Unit>() {
+            val db = UserDataBase.getDatabase(loginActivity, null).userDataDao()
+            override fun doInBackground(vararg params: Void?) {
+                return db.deleteAll()
 
-            //create stopga
-            ////
+            }
+        }
 
-            val layoutNeeded = findViewById<RelativeLayout>(R.id.users_list_view)
-            val newTable = TableLayout(this)
+        class CreateNewUser(private val loginActivity: LoginActivity) : AsyncTask<User, Int, Unit>() {
+            val db = UserDataBase.getDatabase(loginActivity, null).userDataDao()
+            override fun doInBackground(vararg params: User) {
+                return db.insert(params[0])
 
-            val userNamePicList = u.get()
-            var newRow: TableRow? = null
+            }
+        }
 
-            for (a: User? in userNamePicList!!) {
-                var picV: ImageView?
-                //populate ting
-                if (isFirstRow) {
-                    newRow = TableRow(this)
+        class CheckSize(private val loginActivity: LoginActivity) : AsyncTask<Void, Int, Int>() {
+            val db = UserDataBase.getDatabase(loginActivity, null).userDataDao()
+            override fun doInBackground(vararg params: Void?): Int {
+                return db.getCount()
+            }
+        }
 
-                    /*val spacerView = LayoutInflater.from(this).inflate(R.layout.spacer,null, false)
+        class RecyclerAdapter(private val loginActivity: LoginActivity) :
+            RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
+            var users: List<User>? = null
 
-                    newRow.addView(spacerView)*/
-                }
+            init {
+                val e = GetAllUsers(loginActivity).execute()
 
-                val textV = TextView(this)
-                textV.text = a!!.uName
+                users = e.get()
+            }
 
-                val rL = RelativeLayout(this)
+            override fun onBindViewHolder(viewHolder: ViewHolder, i: Int) {
+                viewHolder.user_image.setImageResource(R.drawable.test)
+                viewHolder.user_name.text = users!![i].uName
+            }
 
-                val card = CardView(this)
-                rL.addView(textV)
+            override fun getItemCount(): Int {
+                return users!!.size
+            }
 
-                if (a.picPath != null) {
-                    picV = ImageView(this)
-                    picV.setImageResource(R.drawable.test)
-                    //picV.setImageBitmap(BitmapFactory.decodeFile(File(filesDir,"test.png").absolutePath))
-                    rL.addView(picV)
-                }
+            override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ViewHolder {
+                val v = LayoutInflater.from(viewGroup.context)
+                    .inflate(R.layout.card_layout, viewGroup, false)
+                return ViewHolder(v)
+            }
 
-                card.addView(rL)
-                newRow?.addView(card)
-                if (isFirstRow)
-                    newTable.addView(newRow)
+            inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-                isFirstRow = !isFirstRow
-                card.setOnClickListener {
-                    GlobalApp.setLogged(a.uName)
-                    Toast.makeText(this, "User " + a.uName + " logged in!", Toast.LENGTH_SHORT).show()
-                    setResult(Activity.RESULT_OK)
-                    finish()
+                var user_image: ImageView = itemView.findViewById(R.id.user_image)
+                var user_name: TextView = itemView.findViewById(R.id.user_name)
+
+                init {
+                    itemView.setOnClickListener {
+
+                        val txt = this@RecyclerAdapter.users!![adapterPosition].uName
+                        GlobalApp.setLogged(txt)
+
+                        Toast.makeText(
+                            loginActivity,
+                            "User $txt Logged In!",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        loginActivity.setResult(Activity.RESULT_OK)
+                        loginActivity.finish()
+                    }
+
                 }
 
             }
-
-            layoutNeeded.addView(newTable)
         }
     }
-
-    /**
-     * Attempts to sign in or register the account specified by the login form.
-     * If there are form errors (invalid email, missing fields, etc.), the
-     * errors are presented and no actual login attempt is made.
-     */
-
-    inner class GetAllUsers : AsyncTask<Void, Int, List<User>>() {
-        val db = UserDataBase.getDatabase(this@LoginActivity, null).userDataDao()
-        override fun doInBackground(vararg params: Void?): List<User> {
-            return db.getAll()
-        }
-
-    }
-
-    inner class CreateNewUser : AsyncTask<User, Int, Unit>() {
-        val db = UserDataBase.getDatabase(this@LoginActivity, null).userDataDao()
-        override fun doInBackground(vararg params: User) {
-            return db.insert(params[0])
-
-        }
-
-    }
-
-    inner class CheckSize : AsyncTask<Void, Int, Int>() {
-        val db = UserDataBase.getDatabase(this@LoginActivity, null).userDataDao()
-        override fun doInBackground(vararg params: Void?): Int {
-            return db.getCount()
-        }
-
-    }
-
 }
